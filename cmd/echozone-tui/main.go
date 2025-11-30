efa223fd5b96ff86fbee8455e57479db628c9671
rhit-lopezmo <lopezmo@rose-hulman.edu>
2025-11-29 19:58:05 -0500

    quick and dirty yt-dlp audio link to mpv playback

--- cmd/echozone-tui/main.go
+++ cmd/echozone-tui/main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"os/signal"
+	"strings"
+	"syscall"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	args := os.Args
+
+	if len(args) < 2 {
+		fmt.Println("usage: echozone-tui <url>")
+		os.Exit(1)
+	}
+
+	url := args[1]
+
+	cmd := exec.Command("yt-dlp", "-g", "-f", "140", url)
+	out, err := cmd.Output()
+	if err != nil {
+		fmt.Println("yt-dlp error:", err)
+		os.Exit(1)
+	}
+
+	audioURL := strings.TrimSpace(string(out))
+
+	ctx, cancel := context.WithCancel(context.Background())
+	cmd = exec.CommandContext(ctx, "mpv", "--no-video", audioURL)
+	cmd.Start()
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-signals
+		cancel() // kills MPV
+		os.Exit(0)
+	}()
+
+	for {
+
+	}
 }
